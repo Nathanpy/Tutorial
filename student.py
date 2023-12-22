f81bcfefed9eb8d3ce071952dfef975e579ed9e5
Nathanpy <sunppnn@163.com>
2023-12-22 18:19:18 -0500

    first commit

---
+++ student.py
@@ -0,0 +1,25 @@
+# from typing import Optional
+
+from person import Person
+from enroll import Enroll
+
+class Student(Person):
+    def __init__(self, first, last, dob, phone, address, international=False):
+        super().__init__(self, first, last, dob, phone, address)
+        self.international = international
+        self.enrolled = []
+
+    def add_enrollment(self, enroll):
+        if not isinstance(enroll, Enroll):
+            raise ValueError
+        
+        self.enrolled.append(enroll)
+
+    def is_on_probation(self):
+        return False
+    
+    def is_part_time(self):
+        return len(self.enrolled) <= 3
+    
+    
+            
